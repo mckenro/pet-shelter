3696d5703d5226a309233cf157414fab3d0708e8
Max Larson <mr.m.larson90@gmail.com>
2017-03-06 09:55:15 -0800

    add outline of js for inputting ne wpet for adoption

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+//backend
+  function NewAnimal(name, type, age, description, photo);
+  this.name = name;
+  this.type = type;
+  this.age = age;
+  this.description = description;
+  this.photo = photo;
+
+  NewAnimal.prototype.post= function(){
+    $("#h2 spot").text(this.name);
+    $("#picSpot").text("<img src ='" + this.photo + "'>");
+    $("#ul spot").append("<li>" + "Type: " + this.type + "</li>");
+    $("#ul spot").append("<li>" + "Age: " + this.age + "</li>");
+    $("#p spot").text(this.description);
+
+  }
+
+
+//frontend
+$(document).ready(function{
+  ("##").submit(function(event){
+    var name = $("#name").val();
+    var type = $("#type").val();
+    var age = $("#age").val();
+    var description = $("#description").val();
+    var photo = $("#photo").val();
+    var inputtedAnimal  = new NewAnimal(name, type, age, description, photo);
+    inputtedAnimal.post();
+  });
+});
